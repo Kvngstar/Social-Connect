01696c4d635c0c7edb901fa4c23eb9dc5c505c04
kvngstar <kvngsley019@gmail.com>
2024-01-22 23:55:39 +0100

    worked on video/audio call UI

---
+++ src/calls/video.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Video() {
+  return (
+    <div className='container'>
+        <div>
+<div>Social Connect</div>
+<div></div>
+<div></div>
+
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
