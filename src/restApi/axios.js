1b80ff67ae959208e179d67a737134998bdd45f2
kvngstar <kvngsley019@gmail.com>
2024-02-14 22:42:07 +0100

    still working on BE

--- src/restApi/axios.js
+++ src/restApi/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
-axios.defaults.baseURL = "http://localhost:3001"
+axios.defaults.baseURL = "http://127.0.0.1:3002"
 axios.defaults.headers.common["x-auth"] = localStorage.getItem("AUTHORIZATION")
-axios.defaults.headers.post["content-type"] ="application/x-www-form-urlencoded"
+axios.defaults.headers.post["content-type"] ="application/json"
 
 export default {
     get: axios.get,
